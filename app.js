1b296080cd7d15bb4faf752ac3727862f9618944
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-09 08:49:31 -0500

    added bar

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#foo').addEventListener('click', onFooClick);
 });
 
+function bar() {
+  console.log('bar');
+}
+
+function onBarClick() {
+  console.log('button bar clicked');
+  bar();
+  alert('Thank you for clicking the button bar');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#bar').addEventListener('click', onBarClick);
+});
